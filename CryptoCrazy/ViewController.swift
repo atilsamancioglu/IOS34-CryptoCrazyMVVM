50f152232bc539d0cc27428c6b97ad7db5b1e07e
Atil Samancioglu <samancioglu.atil@gmail.com>
2020-03-19 16:12:43 +0300

    Initial Commit

---
+++ CryptoCrazy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoCrazy
+//
+//  Created by Atil Samancioglu on 19.03.2020.
+//  Copyright © 2020 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
